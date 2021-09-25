b47fc4434d0d86942ed74663ce470879cae546e7
cryptodeal <jimmydeal16@gmail.com>
2021-09-25 13:40:33 -0500

    fix windicss plugin in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: {
-			plugins: [WindiCSS.default()]
+			plugins: [WindiCSS()]
 		}
 	},
 
