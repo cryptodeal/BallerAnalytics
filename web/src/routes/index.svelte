18e0893e80e32058ab9ee96e41fbbad5082763bb
cryptodeal <jimmydeal16@gmail.com>
2021-12-21 17:34:13 -0600

    update deps

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,3 +1,6 @@
+<script lang="ts">
+</script>
+
 <div class="appContent">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
