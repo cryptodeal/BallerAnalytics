bff499c4d3a07f4692b14f96a095bd4c6a1ad3aa
cryptodeal <jimmydeal16@gmail.com>
2021-12-25 13:09:58 -0600

    attempt to cleanup vercel deployment

--- packages/nba-api-ts/src/db/connect.ts
+++ packages/nba-api-ts/src/db/connect.ts
@@ -2,29 +2,6 @@ import mongoose from 'mongoose';
 import config from '../config';
 import { tmpdir } from 'os';
 import { writeFileSync } from 'fs';
-//import { Buffer } from 'buffer';
-
-const base64ToBlob = (base64Data: string): Blob => {
-	// Split into two parts
-	const parts = base64Data.split(';base64,');
-
-	// Hold the content type
-	const fileType = parts[0].split(':')[1];
-
-	// Decode Base64 string
-	const decodedData = atob(parts[1]);
-
-	// Create UNIT8ARRAY of size same as row data length
-	const uInt8Array = new Uint8Array(decodedData.length);
-
-	// Insert all character code into uInt8Array
-	for (let i = 0; i < decodedData.length; ++i) {
-		uInt8Array[i] = decodedData.charCodeAt(i);
-	}
-
-	// Return BLOB image after conversion
-	return new Blob([uInt8Array], { type: fileType });
-};
 
 export const initConnect = () => {
 	const mongooseURI = `mongodb://${config.MONGO_HOST}:${config.MONGO_PORT}/${config.MONGO_DB}`;
@@ -71,17 +48,10 @@ async function serverlessConnect(mongooseURI: string): Promise<typeof mongoose> 
 			useUnifiedTopology: true
 		};
 
-		// For testing purposes
-		//if (config.VITE_NODE_ENV === 'development') {
-		if (config.VITE_NODE_ENV === 'VercelDevelopment') {
+		if (config.VITE_NODE_ENV === 'VercelDevelopment' || config.VITE_NODE_ENV === 'development') {
 			const certFile = Buffer.from(config.MONGO_CLUSTER_CERT, 'base64');
-			//const tlsCAFile = URL.createObjectURL(certFile);
-			//opts.sslCA = Buffer.from(config.MONGO_CLUSTER_CERT, 'base64');
 			const digitalOceanCert = `${tmpdir()}/ca-certificate.cer`;
 			writeFileSync(digitalOceanCert, certFile);
-			// console.log(`${cwd()}/ca-certificate.cer`);
-			//opts.tlsCAFile = resolve('./ca-certificate.cer');
-
 			opts.tlsCAFile = digitalOceanCert;
 		}
 
