dcdb58e1e2694111f4a555ff3d828e8a3e2c4e6f
cryptodeal <jimmydeal16@gmail.com>
2022-01-10 22:24:54 -0600

    update deploy script; fix cron job?

---
+++ packages/nba-api-ts/src/scripts/cron/index.ts
@@ -0,0 +1,29 @@
+import { CronJob } from 'cron';
+import { importGamesLastWeek } from '../../db/controllers/Game2';
+import { atMinute, combine, everyHour } from 'node-cron-expression';
+const gameWeekCronStr = combine(everyHour(), atMinute(30)).toString();
+
+class DataImportScripts {
+	cronJob: CronJob;
+	constructor() {
+		this.cronJob = new CronJob(gameWeekCronStr, async () => {
+			try {
+				await this.importWeekGames();
+			} catch (e) {
+				console.error(e);
+			}
+		});
+
+		// Start job
+		if (!this.cronJob.running) {
+			this.cronJob.start();
+		}
+	}
+
+	private async importWeekGames() {
+		await importGamesLastWeek();
+	}
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const CronJobs = new DataImportScripts();
--- packages/nba-api-ts/src/scripts/index.ts
+++
@@ -1,4 +0,0 @@
-import { registerController } from 'cron-decorators';
-import path from 'path';
-const __dirname = path.resolve();
-registerController([__dirname + '/cron/*.ts']);
