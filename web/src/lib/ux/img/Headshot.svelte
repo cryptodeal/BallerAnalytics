f3500ae4806cbf3dff46c0706338565137b4c3cd
cryptodeal <jimmydeal16@gmail.com>
2022-01-22 16:03:24 -0600

    update error handling in safari

--- web/src/lib/ux/img/Headshot.svelte
+++ web/src/lib/ux/img/Headshot.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	export let src: string;
 	export let alt: string;
-	function handleError() {
-		console.log('error'); // This never happen even when src is invalid
+	function handleError(e: Event) {
+		console.log('error:', e); // This never happen even when src is invalid
 		src = '/headshotFallback.png';
 	}
 </script>
