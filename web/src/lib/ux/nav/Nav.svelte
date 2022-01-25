41d332f9e32d0d408a6a5f16c8f7f57927c64e54
cryptodeal <jimmydeal16@gmail.com>
2022-01-24 23:08:40 -0600

    update landing page svg animation

--- web/src/lib/ux/nav/Nav.svelte
+++ web/src/lib/ux/nav/Nav.svelte
@@ -3,7 +3,6 @@
 	import { session } from '$app/stores';
 	import ThemeToggle from '$lib/ux/nav/ThemeToggle.svelte';
 	import AuthButton from '$lib/ux/nav/AuthButton.svelte';
-	// import Logo from '$lib/ux/Logo.svelte';
 
 	export let segment: string;
 
