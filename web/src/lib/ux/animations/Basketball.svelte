89c91eaefb8039e4b56fe01e6adb4ea78d46f9c0
cryptodeal <jimmydeal16@gmail.com>
2022-01-29 15:13:17 -0600

    update basketball animation on landing page

--- web/src/lib/ux/animations/Basketball.svelte
+++ web/src/lib/ux/animations/Basketball.svelte
@@ -1,35 +1,48 @@
 <script lang="ts">
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
-	import { browser } from '$app/env';
+	import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+	import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+	import { onMount } from 'svelte';
 	export let height: number;
 	export let width: number;
-	let ballTexture;
+	let basketballObj;
 
-	if (browser) {
-		ballTexture = new THREE.TextureLoader().load('basketball.png');
-	}
+	onMount(() => {
+		new MTLLoader()
+			.setPath('/obj/basketball/')
+			.load('Basketball_size6_SF.mtl', function (materials) {
+				materials.preload();
+				new OBJLoader()
+					.setMaterials(materials)
+					.setPath('/obj/basketball/')
+					.load('Basketball_size6_SF.obj', function (object) {
+						basketballObj = object;
+					});
+			});
+	});
 	let clock = new THREE.Clock();
 	let time = 0;
 	let delta = 0;
-	let ballXRotation = 0;
+	let ballYRotation = 0;
 
 	SC.onFrame(() => {
 		delta = clock.getDelta();
 		time += delta;
-		ballXRotation = time * 2;
+		ballYRotation = time * 1.5;
 	});
 </script>
 
-<SC.Canvas antialias alpha={true} background={null} {height} {width}>
-	{#if ballTexture}
+<SC.Canvas antialias alpha={true} {height} {width}>
+	{#if basketballObj}
 		<SC.Mesh
-			geometry={new THREE.SphereGeometry(2, 17, 17)}
-			material={new THREE.MeshBasicMaterial({
-				map: ballTexture
-			})}
-			rotation={[0, ballXRotation, 0]}
+			position={[0, -3.4, 0]}
+			geometry={basketballObj.children[0].geometry}
+			material={basketballObj.children[0].material}
+			rotation={[0, ballYRotation, 0]}
 		/>
 	{/if}
-	<SC.PerspectiveCamera position={[2, 3, 5]} />
+	<SC.AmbientLight intensity={0.65} />
+	<SC.DirectionalLight intensity={0.2} position={[2, 1, 0]} />
+	<SC.PerspectiveCamera position={[0, 0, 10]} />
 </SC.Canvas>
