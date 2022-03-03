e33d3457830298cf09b2a6b6561080e18d23aee8
cryptodeal <jimmydeal16@gmail.com>
2022-03-02 23:21:12 -0600

    update to fix import of dayjs extensions

--- web/src/lib/ux/Ticker/Game.svelte
+++ web/src/lib/ux/Ticker/Game.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import dayjs from 'dayjs';
-	import utc from 'dayjs/plugin/utc';
-	import timezone from 'dayjs/plugin/timezone';
+	import utc from 'dayjs/plugin/utc.js';
+	import timezone from 'dayjs/plugin/timezone.js';
 	dayjs.extend(utc);
 	dayjs.extend(timezone);
 	import { getMainColor } from 'nba-color';
