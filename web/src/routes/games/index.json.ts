0fdc6208d39694cce7a3f5a803004fef8a62c874
cryptodeal <jimmydeal16@gmail.com>
2022-03-10 15:15:03 -0600

    use dayjs plugin timezone

--- web/src/routes/games/index.json.ts
+++ web/src/routes/games/index.json.ts
@@ -1,14 +1,16 @@
 import { getGamesByDate, getMinMaxDates } from '$lib/data/_db/controllers/games';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
 import customParseFormat from 'dayjs/plugin/customParseFormat.js';
 import type { RequestHandler } from '@sveltejs/kit';
 dayjs.extend(utc);
+dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 dayjs.tz.setDefault('America/New_York');
 
 export const get: RequestHandler = async ({ url }) => {
-	let date = dayjs().utc();
+	let date = dayjs().utc().tz();
 	if (url.searchParams.has('date')) {
 		const [year, month, day] = url.searchParams.get('date').split('-').map(parseInt);
 		const tempDate = date
