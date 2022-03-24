cdcf7f1a2869927b572b475c365142e922d51a0e
cryptodeal <jimmydeal16@gmail.com>
2022-03-23 19:41:11 -0500

    improve contrast of backdrop on /teams route

--- web/src/routes/teams/index.svelte
+++ web/src/routes/teams/index.svelte
@@ -24,7 +24,9 @@
 
 <script lang="ts">
 	import TeamLogo from '$lib/ux/teams/assets/AnyTeamLogo.svelte';
+	import tinycolor from 'tinycolor2';
 	import { MetaTags } from 'svelte-meta-tags';
+	import { invertColor } from '$lib/functions/helpers';
 	import { getMainColor, getSecondaryColor } from 'nba-color';
 	export let teams: Team2Object[];
 </script>
@@ -57,15 +59,20 @@
 <div class="appContent flex">
 	<div class="flex-grow mb-10">
 		{#each teams as { infoCommon, seasons }}
+			{@const mainColor = getMainColor(infoCommon.nbaAbbreviation).hex}
+			{@const secondaryColor = getSecondaryColor(infoCommon.nbaAbbreviation).hex}
+			{@const invertedColor = invertColor(mainColor, true)}
+			{@const backdropBg = tinycolor(invertedColor).setAlpha(0.4).toRgbString()}
 			<div class="container mx-auto my-4">
 				<a sveltekit:prefetch href="teams/{infoCommon.slug}">
 					<div
-						style:--teamBg={getMainColor(infoCommon.nbaAbbreviation).hex}
-						style:--teamBorder={getSecondaryColor(infoCommon.nbaAbbreviation).hex}
+						style:--teamBg={mainColor}
+						style:--teamBorder={secondaryColor}
 						class="teamItem rounded-lg shadow-lg border-2 w-full flex flex-wrap gap-y-4 p-3 md:(flex-row)"
 					>
 						<div
-							class="rounded-lg bg-white backdrop-filter backdrop-blur-lg bg-opacity-35 mx-auto w-1/2 px-2 md:w-1/8"
+							style:--logoBg={backdropBg}
+							class="logoBackdrop rounded-lg mx-auto w-1/2 px-2 md:(w-1/8)"
 						>
 							<TeamLogo {logoModules} slug={infoCommon.slug} />
 						</div>
@@ -73,16 +80,11 @@
 							class="md:(w-7/8 justify-end) w-full mx-auto px-3 flex inline-flex h-auto justify-center"
 						>
 							<div
-								style:--backdropBg={getSecondaryColor(infoCommon.nbaAbbreviation).hex ===
-									'#000000' ||
-								(getSecondaryColor(infoCommon.nbaAbbreviation).hex !== '#000000' &&
-									getSecondaryColor(infoCommon.nbaAbbreviation).hex !== '#ffffff')
-									? 'rgba(255,255,255,0.35)'
-									: 'rgba(0,0,0,0.35)'}
-								style:--fontColor={getSecondaryColor(infoCommon.nbaAbbreviation).hex === '#ffffff'
+								style:--backdropBg={backdropBg}
+								style:--fontColor={backdropBg !== 'rgba(255, 255, 255, 0.4)'
 									? '#ffffff'
 									: '#000000'}
-								class="teamInfo rounded-lg teamItem backdrop-blur-xl backdrop-filter bg-opacity-40 my-auto p-2 text-center font-semibold pt-3 md:text-right"
+								class="teamInfo rounded-lg my-auto p-2 text-center font-semibold pt-3 md:text-right"
 							>
 								<div class="text-2xl leading-tight">
 									{infoCommon.name}
@@ -106,6 +108,10 @@
 </div>
 
 <style>
+	.logoBackdrop {
+		background-color: var(--logoBg);
+	}
+
 	.teamItem {
 		background-color: var(--teamBg);
 		border-color: var(--teamBorder);
@@ -114,5 +120,18 @@
 	.teamInfo {
 		background-color: var(--backdropBg);
 		color: var(--fontColor);
+	}
+
+	@supports ((-webkit-backdrop-filter: none) or (backdrop-filter: none)) {
+		.teamInfo {
+			background-color: var(--backdropBg);
+			color: var(--fontColor);
+			@apply backdrop-filter backdrop-blur-md;
+		}
+
+		.logoBackdrop {
+			background-color: var(--logoBg);
+			@apply backdrop-filter backdrop-blur-md;
+		}
 	}
 </style>
