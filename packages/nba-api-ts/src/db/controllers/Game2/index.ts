77faa9a9d1d4bc8996903d514b75829dfc1f9f97
cryptodeal <jimmydeal16@gmail.com>
2022-03-26 16:23:46 -0500

    attempt to update game count for players???

--- packages/nba-api-ts/src/db/controllers/Game2/index.ts
+++ packages/nba-api-ts/src/db/controllers/Game2/index.ts
@@ -513,12 +513,12 @@ export const importLatestGames = () => {
 			const { year } = league.seasons[i];
 			const games = await getSeasonGames(name, year);
 			const playoffGames = (await getPlayoffGames(name, year)).filter(
-				(g) => g.date.isBefore(dayjs()) && g.date.isAfter(dayjs().subtract(7, 'day'))
+				(g) => g.date.isBefore(dayjs()) && g.date.isAfter(dayjs().subtract(3, 'month'))
 			);
 			const regularSeasonGames = games.filter(
 				(g) =>
 					g.date.isBefore(dayjs()) &&
-					g.date.isAfter(dayjs().subtract(7, 'day')) &&
+					g.date.isAfter(dayjs().subtract(3, 'month')) &&
 					playoffGames.findIndex((p) => p.boxScoreUrl === g.boxScoreUrl) === -1
 			);
 			console.log(regularSeasonGames.length);
