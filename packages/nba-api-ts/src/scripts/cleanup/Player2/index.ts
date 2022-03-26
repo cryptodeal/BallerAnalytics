ed51de54c2454fbe82528e67b5e3be4bf0bdcb71
cryptodeal <jimmydeal16@gmail.com>
2022-03-26 16:16:08 -0500

    fix build error caused by changed function name

--- packages/nba-api-ts/src/scripts/cleanup/Player2/index.ts
+++ packages/nba-api-ts/src/scripts/cleanup/Player2/index.ts
@@ -1,10 +1,10 @@
 import { serverlessConnect } from '../../../index';
-import { importAllPlayerStats } from '../../../db/controllers/Player2';
+import { updateActivePlayersCareerStats } from '../../../db/controllers/Player2';
 import config from '../../../config';
 
 const importAllCareerStats = () => {
 	serverlessConnect(config.MONGO_URI).then(async () => {
-		await importAllPlayerStats();
+		await updateActivePlayersCareerStats();
 	});
 };
 
