4cb84a6a30f0dd3b73df213ea7b87c343231c306
cryptodeal <jimmydeal16@gmail.com>
2022-05-07 16:17:22 -0500

    update styles

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -16,7 +16,6 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
-
 	kit: {
 		adapter: adapter({ external: ['@napi-rs/*'] }),
 		vite: {
