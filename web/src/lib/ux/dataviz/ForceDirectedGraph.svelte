58db385be33525d0c0f0f910ef99b455b35d00f8
cryptodeal <jimmydeal16@gmail.com>
2022-05-17 23:12:08 -0500

    update demo viz

--- web/src/lib/ux/dataviz/ForceDirectedGraph.svelte
+++ web/src/lib/ux/dataviz/ForceDirectedGraph.svelte
@@ -1,49 +1,39 @@
 <script lang="ts">
 	import { forceSimulation, forceLink, forceManyBody, forceCenter } from 'd3-force';
-	import { onMount } from 'svelte';
+	import { browser } from '$app/env';
 	import { select } from 'd3-selection';
 	import forceBoundary from 'd3-force-boundary';
 	import { zoom, zoomIdentity } from 'd3-zoom';
 	import { drag } from 'd3-drag';
 	import { shape, intersect } from 'svg-intersections';
-	// import { Chart, Svg, SvgLine, SvgPolygon } from '@sveltejs/pancake/index.mjs';
-	//import { linearScale } from 'yootils';
 	import { NodeType } from '@balleranalytics/tf-neat';
 
 	import type { CXNData, NodeData } from './types';
-	import type { SimulationNodeDatum } from 'd3-force';
+	import type { SimulationNodeDatum, Simulation } from 'd3-force';
+	import { onMount } from 'svelte';
 
 	export let nodes: { type: NodeType; id: number; label: number }[],
 		cxns: { id: number; enabled: boolean; source: number; target: number; label: string }[];
 
 	let svg,
+		simulation: Simulation<SimulationNodeDatum, CXNData>,
 		width = 1,
 		height = 1,
-		simulation,
-		transform = zoomIdentity.translate(0, 0).scale(4),
-		cxnData: CXNData[] = [],
-		nodeData: NodeData[] = [];
+		transform = zoomIdentity;
 
 	const r = 5;
 
-	$: if (nodes && cxns) startSimulation();
-
-	function startSimulation() {
-		if (simulation) simulation.stop();
-
-		transform.translate(0, 0).scale(4);
-		simulation = forceSimulation(nodes as SimulationNodeDatum[])
-			.force(
-				'link',
-				forceLink(cxns).id((d: any) => d.id)
-			)
-			.force('charge', forceManyBody())
-			.force(
-				'boundary',
-				forceBoundary(-width / 2 + 10, -height / 2 + 10, width / 2 - 10, height / 2 - 10)
-			)
-			.on('tick', simulationUpdate);
-	}
+	$: simulation = forceSimulation(nodes as SimulationNodeDatum[])
+		.force(
+			'link',
+			forceLink(cxns).id((d: any) => d.id)
+		)
+		.force('charge', forceManyBody())
+		.force(
+			'boundary',
+			forceBoundary(-width / 2 + 10, -height / 2 + 10, width / 2 - 10, height / 2 - 10)
+		)
+		.on('tick', simulationUpdate);
 
 	onMount(() =>
 		select(svg)
@@ -60,14 +50,16 @@
 					.scaleExtent([1 / 5, 10])
 					.on('zoom', zoomed)
 			)
-			.call(zoom().transform, transform.translate(0, 0).scale(4))
 	);
 
 	/* TODO: Test repl version of demo w drag */
+	let nodeData: NodeData[] = [],
+		cxnData: CXNData[] = [];
+
 	function simulationUpdate() {
 		simulation.tick();
-		nodeData = [...nodes] as unknown[] as NodeData[];
-		cxnData = [...cxns] as unknown[] as CXNData[];
+		nodes = [...nodes];
+		cxns = [...cxns];
 	}
 
 	function zoomed(currentEvent) {
@@ -104,45 +96,48 @@
 		currentEvent.subject.fx = null;
 		currentEvent.subject.fy = null;
 	}
+
+	$: nodeData = nodes as unknown as NodeData[];
+	$: cxnData = cxns as unknown as CXNData[];
 </script>
 
 <div class="flex mx-auto h-400px w-400px md:(h-600px w-600px) xl:(h-750px w-full)">
 	<div class="chart" bind:clientWidth={width} bind:clientHeight={height}>
 		<svg bind:this={svg} {width} {height} viewBox="{-width / 2} {-height / 2} {width} {height}">
-			<g transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})">
-				{#each cxnData as { source: { x: x1, y: y1 }, target: { x: x2, y: y2 }, label }}
-					{@const lineCx = (x1 + x2) / 2}
-					{@const lineCy = (y1 + y2) / 2}
-					{@const endIntersect = intersect(
-						shape('circle', { cx: x2, cy: y2, r }),
-						shape('line', { x1, y1, x2, y2 })
-					)}
-					{@const endX = endIntersect?.points?.length ? endIntersect.points[0].x : x2}
-					{@const endY = endIntersect?.points?.length ? endIntersect.points[0].y : y2}
-					{@const startIntersect = intersect(
-						shape('circle', { cx: x1, cy: y1, r }),
-						shape('line', { x1, y1, x2, y2 })
-					)}
-					{@const startX = startIntersect?.points?.length ? startIntersect.points[0].x : x1}
-					{@const startY = startIntersect?.points?.length ? startIntersect.points[0].y : y1}
-					{@const angle = Math.atan2(y2 - y1, x2 - x1)}
-					{@const cX = (((lineCx + endX) / 2 + endX) / 2 + endX) / 2}
-					{@const cY = (((lineCy + endY) / 2 + endY) / 2 + endY) / 2}
-					{@const baseX = ((lineCx + cX) / 2 + cX) / 2}
-					{@const baseY = ((lineCy + cY) / 2 + cY) / 2}
-					{@const arrowHead = [
-						{ x: Math.sin(angle) + baseX, y: -Math.cos(angle) + baseY },
-						{ x: -Math.sin(angle) + baseX, y: Math.cos(angle) + baseY },
-						{ x: endX, y: endY },
-						{ x: Math.sin(angle) + baseX, y: -Math.cos(angle) + baseY }
-					]}
-					{@const textProps = {
-						x: lineCx,
-						y: lineCy,
-						// https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/text-anchor
-						'text-anchor': 'start',
-						'font-size': `2px`
-					}}
+			{#each cxnData as { source: { x: x1, y: y1 }, target: { x: x2, y: y2 }, label }}
+				{@const lineCx = (x1 + x2) / 2}
+				{@const lineCy = (y1 + y2) / 2}
+				{@const endIntersect = intersect(
+					shape('circle', { cx: x2, cy: y2, r }),
+					shape('line', { x1, y1, x2, y2 })
+				)}
+				{@const endX = endIntersect?.points?.length ? endIntersect.points[0].x : x2}
+				{@const endY = endIntersect?.points?.length ? endIntersect.points[0].y : y2}
+				{@const startIntersect = intersect(
+					shape('circle', { cx: x1, cy: y1, r }),
+					shape('line', { x1, y1, x2, y2 })
+				)}
+				{@const startX = startIntersect?.points?.length ? startIntersect.points[0].x : x1}
+				{@const startY = startIntersect?.points?.length ? startIntersect.points[0].y : y1}
+				{@const angle = Math.atan2(y2 - y1, x2 - x1)}
+				{@const cX = (((lineCx + endX) / 2 + endX) / 2 + endX) / 2}
+				{@const cY = (((lineCy + endY) / 2 + endY) / 2 + endY) / 2}
+				{@const baseX = ((lineCx + cX) / 2 + cX) / 2}
+				{@const baseY = ((lineCy + cY) / 2 + cY) / 2}
+				{@const arrowHead = [
+					{ x: Math.sin(angle) + baseX, y: -Math.cos(angle) + baseY },
+					{ x: -Math.sin(angle) + baseX, y: Math.cos(angle) + baseY },
+					{ x: endX, y: endY },
+					{ x: Math.sin(angle) + baseX, y: -Math.cos(angle) + baseY }
+				]}
+				{@const textProps = {
+					x: lineCx,
+					y: lineCy,
+					// https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/text-anchor
+					'text-anchor': 'start',
+					'font-size': `2px`
+				}}
+				<g transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})">
 					<line
 						class="stroke-dark-800 dark:stroke-light-200"
 						x1={startX}
@@ -157,40 +152,50 @@
 						points={arrowHead.map(({ x, y }) => `${x},${y}`).join(' ')}
 					/>
 					<text class="fill-blue-500 cursor-not-allowed" {...textProps}>{label}</text>
-				{/each}
+				</g>
+			{/each}
 
-				{#each nodeData as { x: cx, y: cy, type, activation }}
-					{@const nodeTypeProps = {
-						x: cx,
-						y: cy,
-						'text-anchor': 'middle',
-						'font-size': `2px`
-					}}
-					{@const actProps = {
-						x: cx,
-						y: cy + 2,
-						'text-anchor': 'middle',
-						'font-size': `2px`
-					}}
-					<circle
-						class="node cursor-grab"
-						{cx}
-						{cy}
-						{r}
-						style:--fillColor={type === NodeType.INPUT
-							? '#87bdd8'
-							: type === NodeType.OUTPUT
-							? '#daebe8'
-							: '#b7d7e8'}
+			{#each nodeData as { x: cx, y: cy, type, activation }}
+				{@const nodeTypeProps = {
+					x: cx,
+					y: cy,
+					'text-anchor': 'middle',
+					'font-size': `2px`
+				}}
+				{@const actProps = {
+					x: cx,
+					y: cy + 2,
+					'text-anchor': 'middle',
+					'font-size': `2px`
+				}}
+
+				<circle
+					class="node cursor-grab"
+					{cx}
+					{cy}
+					{r}
+					transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
+					style:--fillColor={type === NodeType.INPUT
+						? '#87bdd8'
+						: type === NodeType.OUTPUT
+						? '#daebe8'
+						: '#b7d7e8'}
+				>
+					<title>NodeType: {type}</title>
+				</circle>
+				<text
+					class="cursor-grab"
+					transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
+					{...nodeTypeProps}>{type}</text
+				>
+				{#if activation}
+					<text
+						class="cursor-grab"
+						transform="translate({transform.x} {transform.y}) scale({transform.k} {transform.k})"
+						{...actProps}>{activation}</text
 					>
-						<title>NodeType: {type}</title>
-					</circle>
-					<text class="cursor-grab" {...nodeTypeProps}>{type}</text>
-					{#if activation}
-						<text class="cursor-grab" {...actProps}>{activation}</text>
-					{/if}
-				{/each}
-			</g>
+				{/if}
+			{/each}
 		</svg>
 	</div>
 </div>
