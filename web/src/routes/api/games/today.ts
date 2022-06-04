b4a7424e851ac416e491b766897a366ea3770706
cryptodeal <jimmydeal16@gmail.com>
2022-06-04 14:02:16 -0500

    update to fix api route for games/today

--- web/src/routes/api/games/today.ts
+++ web/src/routes/api/games/today.ts
@@ -2,7 +2,7 @@ import { getTodaysGames } from '$lib/data/_db/controllers/games';
 import type { RequestHandler } from '@sveltejs/kit';
 import type { DailyGames } from '$lib/data/stores/types';
 
-export const get: RequestHandler<DailyGames> = async () => {
+export const get: RequestHandler = async () => {
 	const todaysGames: DailyGames = await getTodaysGames();
 
 	return {
