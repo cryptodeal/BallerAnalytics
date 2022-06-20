3721704ec6e56b0556b944c3aa67f4f156f17cb0
cryptodeal <jimmydeal16@gmail.com>
2022-06-20 14:36:09 -0500

    chase that perfect lighthouse score

--- web/src/lib/ux/Modal.svelte
+++ web/src/lib/ux/Modal.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <!-- Put this part before </body> tag -->
-<input type="checkbox" id={modalId} class="modal-toggle" on:click={onClick} />
+<label for={modalId}>
+	<input type="checkbox" id={modalId} class="modal-toggle" on:click={onClick} />
+</label>
 <label for={modalId} class="modal cursor-pointer">
 	<label class="modal-box relative" for="">
 		<slot name="header" />
